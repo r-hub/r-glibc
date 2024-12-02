67a9b09bdcb02a0ae1703a8aeed332c674b2485a
Gábor Csárdi <csardi.gabor@gmail.com>
2024-12-02 12:01:48 +0100

    Tests in various containers

--- tools/install-bats.sh
+++ tools/install-bats.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+if command -v apt-get; then
+    apt-get update &&
+	apt-get install -y curl patchelf file gcc make libc6-dev g++ gfortran
+elif command -v dnf; then
+    dnf install -y epel-release || true # might be needed, might be not
+    dnf install -y curl patchelf file gcc make glibc-devel gcc-c++ gcc-gfortran
+elif command -v yum; then
+    yum install -y epel-release || true # might be needed, might be not
+    yum install -y curl patchelf file gcc make glibc-devel gcc-c++ gcc-gfortran
+else
+    >&2 echo Cannot install bats on this platform
+    exit 1
+fi
+	
 curl -LO https://github.com/bats-core/bats-core/archive/refs/tags/v1.11.0.tar.gz
 tar xzf v1.11.0.tar.gz
 cd bats-core-*
