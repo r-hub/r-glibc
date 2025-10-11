76d11dcb01d1679ce5896ff50f1d9e7579a3f863
Gábor Csárdi <csardi.gabor@gmail.com>
2025-10-11 22:45:02 +0200

    Support R 3.5.3
    So we can build pak for R 3.5.x.

--- test/test-install-fortran.R
+++ test/test-install-fortran.R
@@ -1,4 +1,4 @@
 R.home()
 dir.create(lib <- tempfile(), recursive = TRUE, showWarnings = FALSE)
-install.packages("nlme", repos = "https://cran.rstudio.com", lib = lib)
-library("nlme", lib.loc = lib)
+install.packages("KernSmooth", repos = "https://cran.rstudio.com", lib = lib)
+library("KernSmooth", lib.loc = lib)
